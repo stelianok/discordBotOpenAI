db67fc4c7b49d1a404199e4db4dc857582077cce
Kauã Steliano <stelianok@gmail.com>
2023-01-22 10:28:43 -0300

    Configured server routes and separeted discord.js responsabilities

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,16 @@
-import { Client, Interaction, GatewayIntentBits } from "discord.js";
-import slashChat from "./commands/slashChat";
-import slashHelp from "./commands/slashHelp";
+import express from "express";
+import "./discord";
 
-import { DISCORD_TOKEN } from "./config/secrets";
+const PORT = process.env.PORT || 5000;
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const app = express();
 
-client.once("ready", async () => {
-  console.log("Bot is ready to accept commands");
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", (request, response) => {
+  return response.sendStatus(200);
 });
 
-client.on("interactionCreate", async (interaction: Interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-
-  const { commandName, options } = interaction;
-
-  if (commandName === "ajuda") {
-    slashHelp(interaction);
-  }
-  else if (commandName === "chat") {
-    const message = options.getString("mensagem");
-
-    if (message) {
-      slashChat(interaction, message);
-    }
-    else {
-      interaction.reply("CADE CADE A PERGUNTA");
-    }
-  }
-});
-
-client.login(DISCORD_TOKEN);
+app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
 
 
