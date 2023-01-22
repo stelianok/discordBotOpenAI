4d7c25168195ec4e8bbf8598e166c2261c058bd4
Kauã Steliano <stelianok@gmail.com>
2023-01-22 09:04:42 -0300

    Installed openai dependency

--- src/config/secrets.ts
+++ src/config/secrets.ts
@@ -6,6 +6,8 @@ const DISCORD_TOKEN = process.env["DISCORD_TOKEN"];
 
 const OPENAI_TOKEN = process.env["OPENAI_TOKEN"];
 
+const OPENAI_ORGANIZATION_ID = process.env["OPENAI_ORGANIZATION_ID"];
+
 const CLIENT_ID = process.env["CLIENT_ID"];
 
 if (!DISCORD_TOKEN) {
@@ -14,6 +16,8 @@ if (!DISCORD_TOKEN) {
   console.error("No Todoist token provided");
 } else if (!CLIENT_ID) {
   console.error("No Client id token provided");
+} else if (!OPENAI_ORGANIZATION_ID) {
+  console.error("No OpenAI organization id provided");
 }
 
-export { DISCORD_TOKEN, OPENAI_TOKEN, CLIENT_ID };
+export { DISCORD_TOKEN, OPENAI_TOKEN, OPENAI_ORGANIZATION_ID, CLIENT_ID };
